d7d10f465a067d581aab9b6802a70651e85f64ad
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:20 +0000

    initial commits

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -4,3 +4,4 @@ export const ERRORS = {
   UNSUPPORTED_NETWORK:
     "Unsupported network selected, please use Futurenet in Freighter", // This error message is displayed when an unsupported network is selected and advises the user to use Futurenet in Freighter.
   FREIGHTER_NOT_AVAILABLE: "Please install Freighter to connect your wallet", // This error message is displayed when Freighter is not installed and prompts the user to install it to connect their wallet.
+  UNABLE_TO_SUBMIT_TX: "Unable to submit transaction", // This error message is displayed when there is an issue with submitting a transaction.
