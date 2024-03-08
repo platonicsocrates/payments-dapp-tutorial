2981bf1b63c60bbb289f93b65f3920def4d2e888
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:09:06 +0000

    Initial commit/update

--- commit_each_line.sh
+++ commit_each_line.sh
@@ -23,3 +23,4 @@ find . -type f \( ! -iname ".gitignore" ! -iname "package.json" ! -iname "yarn.l
 
         # Check if there are changes to commit
         if ! git diff --quiet "$file"; then
+            # Commit the change. Customize the commit message as needed.
