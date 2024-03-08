ac0e4f78e00bb2d07e776a71b52fec99fe943fa7
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:08 +0000

    initial commits

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,3 +1,4 @@
 // Hey there! This is the webpack development configuration file.
 // It's responsible for setting up the development environment for our project.
 
+// We start by importing the necessary modules and configurations.
