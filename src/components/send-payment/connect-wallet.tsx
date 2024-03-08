5e4593da3aa0a4fe8fd4b963b666b24307621649
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:22:35 +0000

    initial commits

--- src/components/send-payment/connect-wallet.tsx
+++ src/components/send-payment/connect-wallet.tsx
@@ -37,3 +37,4 @@ export const ConnectWallet = (props: ConnectWalletProps) => {
         {/* We display the selected network as an option */}
         <option>{props.selectedNetwork}</option>
       </Select>
+
