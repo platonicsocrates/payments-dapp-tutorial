81035fa469c5e24c7315b201c95709fa5c1af57b
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:47 +0000

    initial commits

--- src/components/send-payment/index.tsx
+++ src/components/send-payment/index.tsx
@@ -158,3 +158,4 @@ export const SendPayment = (props: SendPaymentProps) => {
       case 7: {
         // Submit the transaction to the network
         const submit = async () => {
+          setConnectionError(null);
