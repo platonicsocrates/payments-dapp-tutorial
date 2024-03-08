5b03455edae710d004740d4746d935722bd7a397
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:21:01 +0000

    Committing line to connect-wallet.tsx

--- src/components/send-payment/connect-wallet.tsx
+++ src/components/send-payment/connect-wallet.tsx
@@ -2,3 +2,4 @@
 // It's responsible for rendering a form to connect a wallet and send a Soroban payment.
 
 import React from "react";
+import { Button, Heading, Select } from "@stellar/design-system";
