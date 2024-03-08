768aa0cb9dbf0a3491bc2af37b855ed66a2a4310
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:15 +0000

    initial commits

--- src/helpers/network.ts
+++ src/helpers/network.ts
@@ -3,3 +3,4 @@ import { StellarWalletsKit } from "stellar-wallets-kit";
 
 // Defining the NetworkDetails interface which describes the details of a network
 export interface NetworkDetails {
+  network: string; // The name of the network
