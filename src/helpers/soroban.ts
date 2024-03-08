e1847bd82a721b2b6b9e8f3824e7393a2db5b66a
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:27 +0000

    initial commits

--- src/helpers/soroban.ts
+++ src/helpers/soroban.ts
@@ -45,3 +45,4 @@ export const RPC_URLS: { [key: string]: string } = {
 
 // Function to convert an account address to a smart contract value
 export const accountToScVal = (account: string) =>
+  new Address(account).toScVal();
