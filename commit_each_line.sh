660482dc8529b6bc33c588b0425482ce30bacb1f
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:28:57 +0000

    initial commits

--- commit_each_line.sh
+++ commit_each_line.sh
@@ -25,3 +25,4 @@ find . -type f \( ! -iname ".gitignore" ! -iname "package.json" ! -iname "yarn.l
         git commit -m "initial commits"
     done < "$backupFile"
 
+    # Cleanup backup for this file
