9f24d16802d9f1136ba28d4d06e91c22477019f4
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:16:39 +0000

    Updated commit from initial state

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -3,3 +3,4 @@ const webpack = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
