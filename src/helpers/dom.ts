75c215eb2a61a04a636e0c4e9c5249c1b8ddbd3b
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:21:02 +0000

    Committing line to dom.ts

--- src/helpers/dom.ts
+++ src/helpers/dom.ts
@@ -1,12 +1,1 @@
 // This function is used to copy the provided text to the clipboard
-export const copyContent = async (text: string) => {
-  try {
-    // We use the navigator.clipboard.writeText() method to write the text to the clipboard
-    await navigator.clipboard.writeText(text);
-    // If the text is successfully copied, we log a success message to the console
-    console.log("Content copied to clipboard");
-  } catch (err) {
-    // If there is an error while copying the text, we log an error message to the console
-    console.error("Failed to copy: ", err);
-  }
-};
