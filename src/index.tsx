bc0a256a5fdb5c2a210ac35357ff52ddf4c50f21
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:01 +0000

    initial commits

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,4 @@ import ReactDOM from "react-dom/client";
 // Importing the SendPayment component from the "./components/send-payment" file
 import { SendPayment } from "./components/send-payment";
 
+// Importing the CSS styles from the "@stellar/design-system/build/styles.min.css" file
