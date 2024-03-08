ef06759684e8a28ac6614e3bf91c7311f7d40439
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:20:45 +0000

    Committing line to types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,1 @@
 // This line is declaring a module named "stellar-identicon-js".
-// It's like saying, "Hey, I want to use this external library called 'stellar-identicon-js' in my code."
-declare module "stellar-identicon-js";
