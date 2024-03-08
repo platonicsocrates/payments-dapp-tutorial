c454d98bad1f3f0b43c00d12eb1cdf8f1314ebe9
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:15 +0000

    initial commits

--- src/helpers/format.ts
+++ src/helpers/format.ts
@@ -5,3 +5,4 @@ export const truncateString = (str: string) =>
   str ? `${str.slice(0, 5)}…${str.slice(-5)}` : "";
 
 // This function converts the base fee from stroops to XLM (Stellar Lumens)
+export const stroopToXlm = (
