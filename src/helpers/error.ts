6f82d6e5bf8d86a26e1e844650c2ff368ab8cb0e
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:20 +0000

    initial commits

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -3,3 +3,4 @@
 export const ERRORS = {
   UNSUPPORTED_NETWORK:
     "Unsupported network selected, please use Futurenet in Freighter", // This error message is displayed when an unsupported network is selected and advises the user to use Futurenet in Freighter.
+  FREIGHTER_NOT_AVAILABLE: "Please install Freighter to connect your wallet", // This error message is displayed when Freighter is not installed and prompts the user to install it to connect their wallet.
