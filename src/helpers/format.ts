b35d74b1155e1e9b354dc6b6d912bb677869bd05
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:25:00 +0000

    initial commits

--- src/helpers/format.ts
+++ src/helpers/format.ts
@@ -16,3 +16,4 @@ export const stroopToXlm = (
 
 // This function converts XLM (Stellar Lumens) to the base fee in stroops
 export const xlmToStroop = (lumens: BigNumber | string): BigNumber => {
+  if (lumens instanceof BigNumber) {
