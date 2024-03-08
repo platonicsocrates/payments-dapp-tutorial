c6749ab5815330a8f6ab9850162b8decf71f8a98
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:16:27 +0000

    Updated commit from initial state

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,3 +2,4 @@
 const webpack = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
