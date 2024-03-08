9a40e8b2b26695b4a56978e8e5f1cf0eecf6cab6
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:21:08 +0000

    Committing line to error.ts

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -1,10 +1,1 @@
 // This file contains a constant object called ERRORS that stores various error messages as properties.
-
-export const ERRORS = {
-  UNSUPPORTED_NETWORK:
-    "Unsupported network selected, please use Futurenet in Freighter", // This error message is displayed when an unsupported network is selected and advises the user to use Futurenet in Freighter.
-  FREIGHTER_NOT_AVAILABLE: "Please install Freighter to connect your wallet", // This error message is displayed when Freighter is not installed and prompts the user to install it to connect their wallet.
-  UNABLE_TO_SUBMIT_TX: "Unable to submit transaction", // This error message is displayed when there is an issue with submitting a transaction.
-  UNABLE_TO_SIGN_TX: "Unable to sign transaction", // This error message is displayed when there is an issue with signing a transaction.
-  WALLET_CONNECTION_REJECTED: "Wallet connection rejected", // This error message is displayed when the connection to the wallet is rejected.
-};
