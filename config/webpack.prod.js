11a95b7fef88681f9062cf9b3a5cfe072c59cf39
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:22:17 +0000

    initial commits

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,3 +5,4 @@ const I18nextWebpackPlugin = require("i18next-scanner-webpack");
 const { commonConfig } = require("./webpack.common.js");
 
 // Define the available locales
+const LOCALES = ["en", "pt"];
