0546a12e2e88d35e46f0619ef533a8139339e4cf
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:21:06 +0000

    Committing line to soroban.ts

--- src/helpers/soroban.ts
+++ src/helpers/soroban.ts
@@ -164,3 +164,4 @@ export const getTokenSymbol = async (
   const contract = new Contract(tokenId);
   const tx = txBuilder
     .addOperation(contract.call("symbol"))
+    .setTimeout(TimeoutInfinite)
