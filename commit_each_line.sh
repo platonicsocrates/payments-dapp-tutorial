7717070c6ec547cdcac25b5044891fc79820b449
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:25:09 +0000

    initial commits

--- commit_each_line.sh
+++ commit_each_line.sh
@@ -8,3 +8,4 @@ mkdir -p "$backupDir" # Create backup directory if it doesn't exist
 find . -type f \( ! -iname ".gitignore" ! -iname "package.json" ! -iname "yarn.lock" ! -iname "tsconfig.json" ! -iname "babel.config.js" ! -iname ".eslintrc.js" ! -iname "README.md" ! -iname "nginx.conf" ! -path './.git/*' ! -path "./$backupDir/*" ! -path './.idea/*' ! -path './node_modules/*' \) -print0 | while IFS= read -r -d $'\0' file; do
     
     echo "Processing file: $file"
+
