ebe36678bc329218f5e6301e2488b8f1ac065e5e
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:21:00 +0000

    Committing line to payment-destination.tsx

--- src/components/send-payment/payment-destination.tsx
+++ src/components/send-payment/payment-destination.tsx
@@ -1,3 +1,4 @@
 // Hey there! This is a component called PaymentDest. It's responsible for rendering a form where users can choose a payment destination.
 
 import React, { ChangeEvent } from "react";
+import { Button, Heading, Input } from "@stellar/design-system";
