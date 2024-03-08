86a8c1e3825e88fa12b25ee723d042db9f44afa3
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:23:29 +0000

    initial commits

--- src/components/send-payment/payment-destination.tsx
+++ src/components/send-payment/payment-destination.tsx
@@ -1,2 +1,3 @@
 // Hey there! This is a component called PaymentDest. It's responsible for rendering a form where users can choose a payment destination.
 
+import React, { ChangeEvent } from "react";
