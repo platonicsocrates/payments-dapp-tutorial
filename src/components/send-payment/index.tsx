f98f2888ac3c00f27a5b6cb76455fc8fefbb1061
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:22:22 +0000

    initial commits

--- src/components/send-payment/index.tsx
+++ src/components/send-payment/index.tsx
@@ -51,3 +51,4 @@ interface SendPaymentProps {
 export const SendPayment = (props: SendPaymentProps) => {
   // This is only needed when this component is consumed by other components that display a different header
   const hasHeader = props.hasHeader === undefined ? true : props.hasHeader;
+
