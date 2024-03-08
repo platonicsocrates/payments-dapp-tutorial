64fc2cfaaf0462be21c68bab2f6e2466fb55ddba
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:15 +0000

    initial commits

--- src/helpers/network.ts
+++ src/helpers/network.ts
@@ -19,3 +19,4 @@ export const FUTURENET_DETAILS = {
 export const signTx = async (
   xdr: string, // The transaction XDR
   publicKey: string, // The public key of the signer
+  kit: StellarWalletsKit, // The StellarWalletsKit instance
