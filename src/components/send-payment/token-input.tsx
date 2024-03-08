6eee018c75d201c18e6b451d5edb15306fb19f72
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:13 +0000

    initial commits

--- src/components/send-payment/token-input.tsx
+++ src/components/send-payment/token-input.tsx
@@ -1,3 +1,4 @@
 // Hey there! This is a component called TokenInput.
 // It's responsible for rendering an input field and a button to choose a token to send.
 
+import React, { ChangeEvent } from "react";
