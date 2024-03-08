225ee91c063175611533ee2e39da9cdb2304b64f
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:27 +0000

    initial commits

--- src/helpers/soroban.ts
+++ src/helpers/soroban.ts
@@ -27,3 +27,4 @@ export const baseFeeXlm = stroopToXlm(BASE_FEE).toString();
 
 // Mapping of send transaction status
 export const SendTxStatus: {
+  [index: string]: SorobanRpc.Api.SendTransactionStatus;
