cd2cb0cd92b53fc1782f5146edb030f55c78b4b0
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:23:27 +0000

    initial commits

--- src/components/send-payment/confirm-payment.tsx
+++ src/components/send-payment/confirm-payment.tsx
@@ -116,3 +116,4 @@ export const ConfirmPayment = (props: ConfirmPaymentProps) => {
       {/* Button to sign the transaction with Freighter */}
       <div className="submit-row-confirm">
         <Button
+          size="md"
