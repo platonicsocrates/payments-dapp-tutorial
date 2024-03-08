869fb75bffc2490eeb96fa8e15ba49855789853a
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:07 +0000

    initial commits

--- commit_each_line.sh
+++ commit_each_line.sh
@@ -5,3 +5,4 @@ backupDir="./backup_for_commits"
 mkdir -p "$backupDir" # Create backup directory if it doesn't exist
 
 # Exclude paths and files in the find command directly
+find . -type f \( ! -iname ".gitignore" ! -iname "package.json" ! -iname "yarn.lock" ! -iname "tsconfig.json" ! -iname "babel.config.js" ! -iname ".eslintrc.js" ! -iname "README.md" ! -iname "nginx.conf" ! -path './.git/*' ! -path "./$backupDir/*" ! -path './.idea/*' ! -path './node_modules/*' \) -print0 | while IFS= read -r -d $'\0' file; do
