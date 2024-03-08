d230b3a0601f236b5764a6b978150b2b58b50112
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:00 +0000

    initial commits

--- src/components/send-payment/send-amount.tsx
+++ src/components/send-payment/send-amount.tsx
@@ -1,3 +1,4 @@
 // Hey there! This is a component called SendAmount. It's responsible for rendering a form where users can enter the amount they want to send.
 
 import React, { ChangeEvent } from "react";
+import BigNumber from "bignumber.js";
