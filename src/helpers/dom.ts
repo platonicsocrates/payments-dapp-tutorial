51550c21eda50c49e76da164b3a2ab63d78bc6b0
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:25 +0000

    initial commits

--- src/helpers/dom.ts
+++ src/helpers/dom.ts
@@ -4,3 +4,4 @@ export const copyContent = async (text: string) => {
     // We use the navigator.clipboard.writeText() method to write the text to the clipboard
     await navigator.clipboard.writeText(text);
     // If the text is successfully copied, we log a success message to the console
+    console.log("Content copied to clipboard");
