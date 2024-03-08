d04fe8c276cf35a88a4c77f01fe04fa0bdeed51a
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:26 +0000

    initial commits

--- src/helpers/format.ts
+++ src/helpers/format.ts
@@ -2,3 +2,4 @@ import BigNumber from "bignumber.js";
 
 // This function truncates a string and adds ellipsis (...) in the middle
 export const truncateString = (str: string) =>
+  str ? `${str.slice(0, 5)}…${str.slice(-5)}` : "";
