c74b0540bc6c17972eb018628abce2a600388a3f
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:22 +0000

    initial commits

--- src/components/send-payment/confirm-payment.tsx
+++ src/components/send-payment/confirm-payment.tsx
@@ -95,3 +95,4 @@ export const ConfirmPayment = (props: ConfirmPaymentProps) => {
             {/* Display recipient's profile picture */}
             <Profile isShort publicAddress={props.destination} size="sm" />
           </div>
+        </div>
