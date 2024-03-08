57114e0167dcaa893dfb0dcd68bf04181e5cddab
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:15 +0000

    initial commits

--- src/helpers/dom.ts
+++ src/helpers/dom.ts
@@ -1,3 +1,4 @@
 // This function is used to copy the provided text to the clipboard
 export const copyContent = async (text: string) => {
   try {
+    // We use the navigator.clipboard.writeText() method to write the text to the clipboard
