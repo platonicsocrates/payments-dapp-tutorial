5320737ade9cf00ed957c81d78d7d5a54ad428a8
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:09 +0000

    initial commits

--- src/components/send-payment/fee.tsx
+++ src/components/send-payment/fee.tsx
@@ -20,3 +20,4 @@ export const Fee = (props: FeeProps) => {
     props.setFee(event.target.value); // Call the setFee function with the new fee value
   };
 
+  // This function handles the change event for the memo input field.
