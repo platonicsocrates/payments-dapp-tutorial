c250137557d01b2155a6f02f2094bc6d60ab12fd
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:29:10 +0000

    initial commits

--- src/components/send-payment/confirm-payment.tsx
+++ src/components/send-payment/confirm-payment.tsx
@@ -1,1 +1,2 @@
 // This component is responsible for rendering the confirmation screen for a payment transaction.
+// It receives various props including the amount, destination, fee, public key, and other necessary details.
