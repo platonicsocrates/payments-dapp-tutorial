a3a291334bad0376a6c02bd5809fdf14b18f47a5
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:20:40 +0000

    Committing line to webpack.prod.js

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,3 +3,4 @@ const { merge } = require("webpack-merge");
 const webpack = require("webpack");
 const I18nextWebpackPlugin = require("i18next-scanner-webpack");
 const { commonConfig } = require("./webpack.common.js");
+
