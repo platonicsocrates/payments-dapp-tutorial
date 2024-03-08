92d6954dff66a8789e48f7fa5b00cd9cd375785b
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:16 +0000

    initial commits

--- src/components/send-payment/index.tsx
+++ src/components/send-payment/index.tsx
@@ -296,3 +296,4 @@ export const SendPayment = (props: SendPaymentProps) => {
             // Open the wallet modal and set the selected wallet, network, and public key
             await SWKKit.openModal({
               allowedWallets: [
+                WalletType.ALBEDO,
