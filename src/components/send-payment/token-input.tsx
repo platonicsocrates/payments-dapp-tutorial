73bd436bed5553604af72bf2affaabf82a9d7e98
platonicsocrates <47001602+platonicsocrates@users.noreply.github.com>
2024-03-08 14:24:57 +0000

    initial commits

--- src/components/send-payment/token-input.tsx
+++ src/components/send-payment/token-input.tsx
@@ -12,3 +12,4 @@ interface TokenInputProps {
 // Now, let's define the TokenInput component.
 export const TokenInput = (props: TokenInputProps) => {
   // We're using React hooks here to manage the state of the input field.
+  // The value of the input field is stored in the 'value' state variable.
